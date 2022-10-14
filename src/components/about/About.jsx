741acd5a0bd7c923dc0cde166436d20cd2a115c9
Rahwa <rahwafesseha@gmail.com>
2022-10-14 23:42:34 +0100

    experience section added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 import "./about.css"
 
+
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div>
+        <article>
+          
+          <p>
+            I am a passionate and self-driven Full Stack Developer. After
+            earning a degree in marine engineering and working as a Maths tutor,
+            I later decided to study network administration because I have
+            always been interested in IT. However, it was during lockdown that I
+            discovered my true passion and began to learn to code. I built
+            full-stack web applications using technologies such as HTML, CSS,
+            JavaScript, react, node, and PostgreSQL both independently and in
+            teams. I appreciate using technology to find solutions to problems
+            in the real world, and I like CISCO and the hardware and software
+            that form the foundation of network communication.
+          </p>
+        </article>
+        <a href="#contact" className="btn about_me"> Reach Me...</a>
+      </div>
+    </section>
+  );
 }
 
 export default About
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,53 @@
-import React from 'react';
-import "./experience.css"
+import React from "react";
+import "./experience.css";
+import { SiHtml5 } from "react-icons/si";
+import { DiCss3 } from "react-icons/di";
+import { TbBrandJavascript } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
+import { GrMysql } from "react-icons/gr";
+import { FaNodeJs } from "react-icons/fa";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
-}
+  return (
+    <section id="experience">
+      <h5>The Skills I have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience_container">
+        <div>
+          <h3>Front-end Technologies</h3>
+          <div>
+            <article>
+              <SiHtml5 />
+              <h4>HTML</h4>
+            </article>
+            <article>
+              <DiCss3 />
+              <h4>CSS</h4>
+            </article>
+            <article>
+              <TbBrandJavascript />
+              <h4>JavaScript</h4>
+            </article>
+            <article>
+              <FaReact />
+              <h4>REACT</h4>
+            </article>
+          </div>
+        </div>
+        <div>
+          <h3>Back-end Technologies</h3>
+          <article>
+            <GrMysql />
+            <h4>MySQL</h4>
+          </article>
+          <article>
+            <FaNodeJs />
+            <h4>Node.js</h4>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Experience
+export default Experience;
