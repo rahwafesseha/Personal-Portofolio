c8df4419d5b852ddc10b5643c1685c9d820192fb
Rahwa <rahwafesseha@gmail.com>
2023-01-04 11:59:06 +0000

    submit form with emailjs added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './components/header/Header';
 import Nav from "./components/nav/Nav";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
-import Services from './components/services/Services';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
@@ -16,10 +15,9 @@ const App = () => {
       <Nav />
       <About />
       <Experience />
-      <Services/>
       <Contact />
       <Footer />
-      <map/>
+    
     </>
   );
 }
